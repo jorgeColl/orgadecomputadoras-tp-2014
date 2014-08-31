8f2a6c041c878750b29c5e2705c050ba28fbcc6b
llinetjorge <collinetjorge@gmail.com>
2014-08-31 20:12:18 +0000

    carga de opciones al 90% mustreo de lineas al 20%

--- main.c
+++ main.c
@@ -2,144 +2,157 @@
  * main.c
  *
  *  Created on: 29/08/2014
- *      Author: jorge
+ *      Author: jorge y jasmina y ....
  */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <getopt.h>
-/*
- * -s, --number-separator
- * -v, --starting-line-number
- * -i, --line-increment
- * -t, --non-empty
- * -l, --join-blank-lines
- * -h, --help
- *
- */
+#include <stdbool.h>
 
+static bool DEBUG =true;
+static char mensaje_ayuda[]=""
+ "* -s, --number-separator	(Indica el texto separador entre numero de lınea y la lınea).\n"
+ "* -v, --starting-line-number	(Indica el numero de la primer lınea).\n"
+ "* -i, --line-increment	(Indica el incremento entre lıneas consecutivas).\n"
+ "* -t, --non-empty		(Si esta presente, solo se deben numerar las lıneas NO vacias. Caso con-trario, tambien deben numerar las lıneas vacias).\n"
+ "* -l, --join-blank-lines	(Indica la cantidad de lıneas vacias a agrupar en una unica lınea).\n"
+ "* -h, --help			(Imprime el mensaje de ayuda).\n";
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <getopt.h>
 // optarg: opcion de argumento ej --add=gg  -> optarg = gg
-/* Flag set by ‘--verbose’. */
-static int verbose_flag;
+
 static struct option long_options[] = {
-		/* These options set a flag. */
-		{ "verbose", no_argument, &verbose_flag, 1 },
-		{ "brief", no_argument,&verbose_flag, 0 },
-		/* These options don't set a flag. We distinguish them by their indices. */
-		{ "number-separator", 	required_argument, 0, 's' },
-		{ "starting-line-number", required_argument, 0, 'v' },
-		{ "line-increment", 	required_argument, 0, 'i' },
-		{ "non-empty",			no_argument, 0, 't' },
-		{ "join-blank-lines", 	required_argument, 0, 'l' },
-		{ "help", 				no_argument, 0, 'h' },
+		/* para cada opcion, se registra si necesita argumento y que letra devuelve getopt_long(), en caso
+		 * de encontrarse con esa opcion */
+		{ "number-separator", 	required_argument, 		0, 's' },
+		{ "starting-line-number", required_argument, 	0, 'v' },
+		{ "line-increment", 	required_argument, 		0, 'i' },
+		{ "non-empty",			no_argument, 			0, 't' },
+		{ "join-blank-lines", 	required_argument, 		0, 'l' },
+		{ "help", 				no_argument, 			0, 'h' },
+		// esto lo demanda la funcion
 		{ 0, 0, 0, 0 }
 };
-int main(int argc, char **argv) {
+
+static char short_options[] = "s:v:i:t:l:h";
+static char number_peratator[10];
+static unsigned long starting_line_number = 0;
+static int line_increment = 0;
+static bool non_empty = false;
+static unsigned int join_blank_lines = 1;
+
+/*
+ * Funcion encargada de la carga de iopciones proveniente de la llamada al programa
+ */
+void init(int argc, char **argv) {
 	/* getopt_long stores the option index here. */
 	int option_index = 0;
-	int c = getopt_long(argc, argv, "s:v:i:t:l:h", long_options, &option_index);
-	while (c!=-1) {
-		switch (c) {
-		case 0:
-			/* If this option set a flag, do nothing else now. */
-			if (long_options[option_index].flag != 0)
-				break;
-			printf("option %s", long_options[option_index].name);
-			if (optarg)
-				printf(" with arg %s", optarg);
-			printf("\n");
-			break;
+	int c = getopt_long(argc, argv, short_options, long_options, &option_index);
+	while (c != -1) {
+		//pequeña correccion para que no tenga en cuenta el '=' como valor en el argumento
+		// si se pasa el argumento de forma -s="jjoj"
+		if(optarg!= NULL && optarg[0]=='=' && optarg[1]!='\0'){
+			optarg++;
+		}
 
+		switch (c) {
 		case 's':
 			printf("option -s o number-separator con valor: %s\n", optarg);
+			strcpy(number_peratator, optarg);
 			break;
 
 		case 'v':
-			printf("option -v o --starting-line-number con valor: %s\n", optarg);
+			printf("option -v o --starting-line-number con valor: %s\n",optarg);
+			starting_line_number = atol(optarg);
 			break;
 
 		case 'i':
 			printf("option -i o --line-increment con valor: %s\n", optarg);
+			line_increment = atoi(optarg);
 			break;
 
 		case 't':
-			printf("option -t o --non-empty con valor: %s\n", optarg);
+			printf("option -t o --non-empty\n");
+			non_empty = true;
 			break;
 
 		case 'l':
 			printf("option -l o --join-blank-lines con valor: %s\n", optarg);
+			join_blank_lines = atoi(optarg);
 			break;
 
 		case 'h':
-			printf("option -h o --help");
+			printf("option -h o --help\n");
+			printf("%s", mensaje_ayuda);
 			break;
 
 		default:
-			printf("no se entiende:%c\n",c );
 			abort();
 		}
-		c = getopt_long(argc, argv, "a:b:c:d:f:", long_options, &option_index);
+
+		c = getopt_long(argc, argv, short_options, long_options, &option_index);
 	}
 
-	/* Instead of reporting ‘--verbose’ and ‘--brief’ as they are encountered, we report the final status resulting from them. */
-	if (verbose_flag)
-		puts("verbose flag is set");
 
+}
+
+void escribir_archivo_en_stdout(FILE* fd) {
+	char c = fgetc(fd);
+	while (c != EOF) {
+		printf("%c", c);
+		c = fgetc(fd);
+	}
+}
+
+void procesar_archivos(int optind, int argc, char* argv[]) {
 	/* Print any remaining command line arguments (not options). */
 	if (optind < argc) {
-		printf("elementos que no pudieron ser procesados: ");
-		while (optind < argc)
-			printf("%s ", argv[optind++]);
-		putchar('\n');
+		int aux = optind;
+		printf("elementos que se consideran archivos: \n");
+		while (optind < argc) {
+			printf("%s\n", argv[optind++]);
+		}
+		optind = aux;
+		while (optind < argc) {
+			char temp[50];
+			if (argv[optind][0] == '-') {
+				printf("123456789\n");
+				while(!feof(stdin)){
+
+					scanf("%s",temp);
+					printf("%s\n",temp);
+				}
+			} else {
+				FILE* fd = fopen(argv[optind], "r");
+				if (fd == NULL) {
+					fprintf(stderr, "Error al tratar de abrir el archivo:'%s' ",
+							argv[optind]);
+					perror("");
+				} else {
+					escribir_archivo_en_stdout(fd);
+				}
+			}
+			optind++;
+		}
+		printf("\n");
+	}
+}
+
+int main(int argc, char **argv) {
+
+	init(argc, argv);
+
+	if (DEBUG) {
+		printf("++++++++++++++++++\nDEBUG IS ON\n");
+		printf("number_peratator:'%s'\n", number_peratator);
+		printf("starting_line_number:'%lu'\n", starting_line_number);
+		printf("line_increment:'%i'\n", line_increment);
+		printf("non_empty:'%s'\n", (non_empty == 0) ? "true" : "false");
+		printf("join_blank_lines:'%i'\n", join_blank_lines);
+		printf("++++++++++++++++++\n");
 	}
 
+	procesar_archivos(optind,argc,argv);
+
 	exit(0);
-} /*
- char separador[10];
- separador[1]=' ';
- int v=0;
- for (int i = 0; i < argc; i++) {
- char* input = argv[i];
- printf("input: %s\n",input);
- if (input[0] == '-' && input[1] != '-' && input[2]=='=') {
- printf("opcion corta:%s\n",input);
- switch (input[1]) {
- case 's':
- strcpy(separador,input);
- printf("SEPARADOR: %s\n",separador);
- break;
- case 'v':
- v=atoi(input[3]);
- break;
- case 'i':
-
- break;
- case 't':
-
- break;
- case 'l':
-
- break;
- case 'h':
- default:
- printf("comandos posibles:\n"
- "* -s, --number-separator\n"
- "* -v, --starting-line-number\n"
- "* -i, --line-increment\n"
- "* -t, --non-empty\n"
- "* -l, --join-blank-lines\n"
- "* -h, --help\n");
-
- break;
- }
- }
- if (input[0] == '-' && input[1] == '-') {
- printf("opcion larga:%s\n",input);
- }
- printf("%s\n", input);
- }*/
-
+}
