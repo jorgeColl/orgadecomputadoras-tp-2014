cf40ac874a4626faf8d1717f667587e4c0166d7d
llinetjorge <collinetjorge@gmail.com>
2014-11-06 00:29:30 +0000

    solo falta write error

--- TP1/validate_mips32/main.c
+++ TP1/validate_mips32/main.c
@@ -2,56 +2,101 @@
 #include <stdio.h>
 
 int validate(char* text, char** errmsg);
-/*
-void aux (char* y, int esperado){
-	char* errmsg = NULL;
-	int resultado = validate(y, &errmsg);
-	printf("texto:\n%s\n", y);
-	printf("mensaje de error: %s\n",errmsg);
-	
-	if (resultado==esperado){
-		printf("FAIL\n");
-	}else{
-		printf("OK\n");
-	}
-}*/
+
 #define MALXML 0
 #define BIENXML 1
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
 
-int main(int argc,char **argv) {
-	
-	
-	//char* a ="<sss><\\sss>"; DIOS SANTO PODEROSO, NO PUEDO HUMANAMENTE COMPRENDER PORQUE SI DESCOMENTO ESTA LINEA,
-	// EL PRINT DE UNA DE LAS PRUEBAS NO ANDA, Y LA PRUEBA TAMBIEN DA CUALQUIER COSA
-	// mmm podria ser por el parche asqueroso que use adentro del assembly
-	char* b ="<z>xx<w>xxx<d>xxx<\\z><fofo><pepe>";
-	char* c ="<abaaaac>xxx\n\n\nxx<\\abaaaac><yoyo><\\yoyo><wewewwew>\n\ngrgrgr<\\wewewwew>";
-	char* d ="<x><\\x><aza>xxx\n<sese>xxx<sisi><y><\\y>";
-	char* e ="<jojo><\\jojo>xxxx<\\aaaa>";
-	
-	
-	printf("leyenda:\n-666 -> HUBO ALGUN ERROR\n-111 -> tag mal anidado\n-888 -> tags sin cerrar\n-222 -> tag sin abir\n-999 -> son distintos pero falta saber porque\n  1  -> iguales\n\n");
-	/*aux(a,BIENXML);
-	aux(b,MALXML);
-	aux(c,MALXML);
-	aux(d,MALXML);*/
-	
-	char* errmsg = NULL;
-	int resultado;
-	//int resultado = validate(a, &errmsg);
-	//printf("PRUEBA 1: %s resultado:%d\n",a,resultado);
-	
-	resultado = validate(b, &errmsg);
-	printf("PRUEBA 2:\n%s resultado:%d\n",b,resultado);
-	
-	resultado = validate(c, &errmsg);
-	printf("\nPRUEBA 3:\n%s resultado:%d\n",c,resultado);
-	
-	resultado = validate(d, &errmsg);
-	printf("\nPRUEBA 4:\n%s resultado:%d\n",d,resultado);
-	
-	resultado = validate(e, &errmsg);
-	printf("\nPRUEBA 5:\n%s resultado:%d\n",e,resultado);
 
-	return 0;
+static struct option long_options[] = {
+		/* para cada opcion, se registra si necesita argumento y que letra devuelve getopt_long(), en caso
+		 * de encontrarse con esa opcion */
+		{ "input",		required_argument,	0, 'i' },
+		{ "version", 	no_argument,		0, 'v' },
+		{ "help", 		no_argument,		0, 'h' },
+		// esto lo demanda la funcion
+		{ 0, 0, 0, 0 }
+};
+
+static char short_options[] = "hvi:";
+
+static char mensaje_de_ayuda[]="Usage:\n"
+		" validate -h\n"
+		" validate -V \n"
+		" validate [options] file\n"
+		"Options:\n"
+		" -h, --help Prints usage information.\n"
+		" -V, --version Prints version information.\n"
+		" -i, --input Path to input file (-i - for stdin)\n"
+		"Examples:\n"
+		" validate -i -\n"
+		" validate myfile.tagged\n"
+		" validate -i myfile.tagged\n";
+/**
+ * Funcion encargada de cargar todo el archivo en un char*
+ * Esto se hace asumiendo que todo el archivo entra en memoria
+ */
+char* cargar_archivo(FILE* fd) {
+	long TAM = 5;
+	char* buff = (char*) malloc(sizeof(char) * TAM);
+	long cont = 0;
+	while (!feof(fd)) {
+		char c = fgetc(fd);
+		buff[cont] = c;
+		cont++;
+		if (cont == TAM) {
+			TAM = 2 * TAM;
+			buff = realloc(buff, TAM);
+		}
+	}
+	buff[cont - 1] = '\0';
+	fclose(fd);
+	return buff;
+}
+
+int main(int argc, char **argv) {
+	FILE* fd = NULL;
+	int option_index = 0;
+	int c = getopt_long(argc, argv, short_options, long_options, &option_index);
+	switch (c) {
+	case 'h':
+		printf("%s", mensaje_de_ayuda);
+		return EXIT_SUCCESS;
+
+	case 'v':
+		printf("Version 1.00\n");
+		return EXIT_SUCCESS;
+
+	case 'i':
+		if (optarg[0] == '-' && optarg[1] == '\0') {
+			fd = stdin;
+		} else {
+			fd = fopen(optarg, "r");
+			if (fd == NULL) {
+				fprintf(stderr, "%s", "error al abrir el archivo\n");
+				perror("");
+				return EXIT_FAILURE;
+			}
+		}
+
+		break;
+	default:
+		break;
+	}
+	char* buff = cargar_archivo(fd);
+	char* error = NULL;
+	
+	int resultado = validate(buff,&error);
+	printf("resultado: %d\n",resultado);
+	//if(resultado == 1){
+	//	fprintf(stderr,"%s",error);
+	//	free(error);
+	//}
+	free(buff);
+	return resultado;
+		
 }
